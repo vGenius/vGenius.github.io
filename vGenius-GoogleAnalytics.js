8de9ebbd0fdd781af83fb5c002b7d9a9ee7a652a
vGenius <jenie@vgenius.co.in>
2015-04-11 06:40:18 +0800

    Update vGenius-GoogleAnalytics.js
    Removed script tag & added comments

--- vGenius-GoogleAnalytics.js
+++ vGenius-GoogleAnalytics.js
@@ -1,10 +1,9 @@
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+//This code is loaded by a script tag from another file in multiple pages hence removed the script tage in this page
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-61564450-1', 'auto');
   ga('send', 'pageview');
-
-</script>
+ 
