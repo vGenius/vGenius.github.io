40cb0dfabe7aec5dd6c63cb235b4eb055add4a01
aruljenie <aruljenie@gmail.com>
2015-07-26 15:25:19 +0800

    Editing HTML to form template for vGenius

--- vGenius-GoogleAnalytics.js
+++ vGenius-GoogleAnalytics.js
@@ -1,5 +1,6 @@
 //This code is loaded by a script tag from another file in multiple pages hence removed the script tage in this page
 <script>
+    alert('Google Analytics Loaded');
  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -8,4 +9,3 @@
   ga('create', 'UA-61564450-1', 'auto');
   ga('send', 'pageview');
   </script>
- 
