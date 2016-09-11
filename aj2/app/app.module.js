4002d5fc8270a2defbbf62044605d7bf5b779e23
vGenius <jenie@vgenius.co.in>
2016-09-11 09:59:22 +0800

    Add files via upload
    Angular Js Hello World

---
+++ aj2/app/app.module.js
@@ -0,0 +1,18 @@
+(function(app) {
+  app.AppModule =
+    ng.core.NgModule({
+      imports: [ ng.platformBrowser.BrowserModule ],
+      declarations: [ app.AppComponent ],
+      bootstrap: [ app.AppComponent ]
+    })
+    .Class({
+      constructor: function() {}
+    });
+})(window.app || (window.app = {}));
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
